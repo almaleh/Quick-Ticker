dedcaaa506c49835afb916f8f2f429fd190c6a0e
BesherAlMaleh <besher.m@gmail.com>
2018-10-16 21:58:04 -0400

    Initial Commit

---
+++ Ticker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ticker
+//
+//  Created by Besher on 2018-10-16.
+//  Copyright © 2018 Besher Al Maleh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
