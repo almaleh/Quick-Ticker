c23f7f20c6df8fc10cf4742da06260ddbf584d27
BesherAlMaleh <besher.m@gmail.com>
2018-10-21 11:57:37 -0400

    add generic parameters for end value and label. detect decimal count from input

--- Ticker/Ticker.swift
+++ Ticker/Ticker.swift
@@ -14,21 +14,29 @@ struct QTickerOptions {
     
 }
 
-enum QTickerCurve {
-    case linear, easeIn, easeOut
+protocol TextLabel: class {
+    var text: String? { get set }
 }
 
-fileprivate class QTickerAnimation {
-    lazy var startValue = getStartingValue(from: animationLabel?.text ?? "")
-    let startTime = Date()
-    weak var animationLabel: UILabel?
+
+fileprivate class QTickerAnimation<T: QTNumericValue> {
     
+    let animationStartTime = Date()
+    lazy var animationStartValue = getStartingValue(from: animationLabel?.text ?? "")
+    var requiredNumberOfDecimals: Int {
+        let startValueCount = getDecimalCount(input: animationStartValue)
+        let endValueCount = getDecimalCount(input: Double(fromNumeric: animationEndValue))
+        return max(startValueCount, endValueCount)
+    }
+    
+    // The following properties are set during initialization
+    weak var animationLabel: TextLabel?
     var animationDisplayLink: CADisplayLink? = nil
-    let animationEndValue: Int
-    let animationCurve: QTickerCurve
+    let animationEndValue: T
+    let animationCurve: QuickTicker.AnimationCurve
     let animationDuration: TimeInterval
     
-    init(label: UILabel, duration: TimeInterval, endValue: Int, curve: QTickerCurve) {
+    init(label: TextLabel, duration: TimeInterval, endValue: T, curve: QuickTicker.AnimationCurve) {
         animationLabel = label
         animationDuration = duration
         animationCurve = curve
@@ -38,63 +46,185 @@ fileprivate class QTickerAnimation {
     }
     
     @objc private func handleUpdate() {
+        handleUpdateFor(animationLabel, startTime: animationStartTime, animationDuration: animationDuration,
+                        startValue: animationStartValue, endValue: animationEndValue, curve: animationCurve,
+                        displayLink: animationDisplayLink)
+    }
+    
+    private func handleUpdateFor(_ label: TextLabel?, startTime: Date, animationDuration: TimeInterval,
+                                 startValue: Double, endValue: T, curve: QuickTicker.AnimationCurve,
+                                 displayLink: CADisplayLink?) {
+        
+        var animationDisplayLink = displayLink
         
         let elapsedTime = Date().timeIntervalSince(startTime)
         
         if elapsedTime <= animationDuration {
             let percentage = elapsedTime / animationDuration
-            let value = getValueFromPercentage(percentage, startValue: startValue, endValue: animationEndValue, curve: animationCurve)
-            
-            if (value != animationEndValue) {
-                animationLabel?.text = String(value)
+            let value = getValueFromPercentage(percentage, startValue: startValue, endValue: endValue, curve: curve)
+            print(value)
+            if (value != Double(fromNumeric: animationEndValue)) {
+                updateLabel(label, withValue: value, numberOfDecimals: requiredNumberOfDecimals)
+                print(String(value))
             }
         } else {
             animationDisplayLink?.invalidate()
             animationDisplayLink = nil
-            animationLabel?.text = String(self.animationEndValue)
+            updateLabel(label, withValue: Double(fromNumeric: endValue), numberOfDecimals: requiredNumberOfDecimals)
         }
     }
     
-    private func getStartingValue(from start: String) -> Int {
-        let filtered = start.compactMap { Int(String($0)) }
+    private func updateLabel(_ label: TextLabel?, withValue value: Double, numberOfDecimals: Int) {
+        
+        print("Required number is \(requiredNumberOfDecimals)")
+        
+        let power = pow(10, Double(numberOfDecimals))
+        let updatedValue = Double(round(power * value) / power)
+        
+        if numberOfDecimals == 0 {
+            label?.text = String(Int(updatedValue))
+        } else {
+            label?.text = String(updatedValue)
+        }
+        
+        
+        
+//        // if no decimals, we return Int value to avoid zeroes
+//        if digitIsInt(value) {
+//            label?.text = String(Int(value))
+//            print("No decimals")
+//        }
+//
+//        // TODO replace with simpler rounding
+//
+//        print("Yes decimals")
+//        // Output with desired decimal count
+//        let text = String(value)
+//        if let dot = text.index(of: ".") {
+//            if let index = text.index(dot, offsetBy: numberOfDecimals,
+//                                      limitedBy: text.index(text.endIndex, offsetBy: -1)) {
+//                if index == dot {
+//                    label?.text = String(Int(value))
+//                } else {
+//                    let firstPart = text[text.startIndex...index]
+//                    label?.text = String(firstPart)
+//                }
+//            }
+//        }
+    }
+    
+    private func getStartingValue(from start: String) -> Double {
+        
+        if let digit = Double(start) {
+            return digit
+        }
+        
+        
+        
+        let filtered = start.compactMap { Double( String($0)) }
         let joined = filtered.reduce(0) { $0 * 10 + $1 }
+        print("Start was \(start) and end was \(joined)")
         return joined
     }
     
-    private func getValueFromPercentage(_ percentage: Double, startValue: Int, endValue: Int, curve: QTickerCurve) -> Int {
-        
+    private func digitIsInt(_ digit: Double) -> Bool {
+        return Double(Int(digit)) == digit
+    }
+    
+    private func getValueFromPercentage(_ percentage: Double, startValue: Double, endValue: T, curve: QuickTicker.AnimationCurve) -> Double {
+        let endDouble = Double(fromNumeric: endValue)
         switch curve {
-        case .linear: return startValue + Int(percentage * Double((endValue - startValue)))
+        case .linear: return startValue + (percentage * (endDouble - startValue))
             
         case .easeOut:
             var curvedPercentage = 1 - percentage
-            curvedPercentage = 1 - pow(curvedPercentage, 2.5)
-            return startValue + Int(curvedPercentage * Double((endValue - startValue)))
+            curvedPercentage = 1 - pow(curvedPercentage, 2.8)
+            return startValue + (curvedPercentage * (endDouble - startValue))
             
         case .easeIn:
-            let curvedPercentage = pow(percentage, 2.5)
-            return startValue + Int(curvedPercentage * Double((endValue - startValue)))
+            let curvedPercentage = pow(percentage, 2.8)
+            return startValue + (curvedPercentage * (endDouble - startValue))
         }
     }
+    
+    private func getDecimalCount(input: Double) -> Int {
+        if digitIsInt(input) { return 0 }
+        let str = String(input)
+        if let dot = str.index(of: ".") {
+            let firstPart = str[str.startIndex...dot]
+            var decimals = str
+            decimals.removeFirst(firstPart.count)
+            return decimals.count
+        } else { return 0 }
+    }
+    
+    
 }
 
-class QTicker {
-    //TODO: Convenience initializers
-    typealias EndValue = Int
+class QuickTicker {
     
-    static func animate(label: UILabel,
-                        duration: TimeInterval = 2,
-                        toEndValue endValue: EndValue,
-                        curve: QTickerCurve = .linear,
-                        completion: (() -> Void)? = nil) {
+    enum AnimationCurve {
+        case linear, easeIn, easeOut
+    }
+    
+    //TODO: Convenience initializers
+    
+    /// Starts a ticker animation on a UILabel or TextField using the provided end value. Optional parameters include duration, curve, and completion handler. 
+    class func animate<T: QTNumericValue, L: TextLabel>(label: L, toEndValue endValue: T, duration: TimeInterval = 2, 
+                        curve: AnimationCurve = .linear, completion: (() -> Void)? = nil) {
         
-        _ = QTickerAnimation(label: label,
-                             duration: duration,
-                             endValue: endValue,
-                             curve: curve)
+        _ = QTickerAnimation(label: label, duration: duration, endValue: endValue, curve: curve)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
             completion?()
         }
     }
+    
+    // TODO: find number of decimal points
+    
+    
+    
 }
+
+// This protocol allows us to cast generic input to other types
+protocol QTNumericValue : Comparable {
+    init(_ v:Float)
+    init(_ v:Double)
+    init(_ v:Int)
+    init(_ v:UInt)
+    init(_ v:Int8)
+    init(_ v:UInt8)
+    init(_ v:Int16)
+    init(_ v:UInt16)
+    init(_ v:Int32)
+    init(_ v:UInt32)
+    init(_ v:Int64)
+    init(_ v:UInt64)
+    init(_ v:CGFloat)
+    
+    // 'shadow method' that allows instances of QTickerNumeric
+    // to coerce themselves to another QTickerNumeric type
+    func _asOther<T:QTNumericValue>() -> T
+}
+
+extension QTNumericValue {
+    init<T:QTNumericValue>(fromNumeric numeric: T) { self = numeric._asOther() }
+}
+
+extension Float   : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension Double  : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension CGFloat : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension Int     : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension Int8    : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension Int16   : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension Int32   : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension Int64   : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension UInt    : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension UInt8   : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension UInt16  : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension UInt32  : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+extension UInt64  : QTNumericValue {func _asOther<T:QTNumericValue>() -> T { return T(self) }}
+
+extension UILabel : TextLabel { }
+extension UITextField: TextLabel { }
+
--- Ticker/ViewController.swift
+++ Ticker/ViewController.swift
@@ -10,19 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var linear: UILabel!
-    @IBOutlet weak var easeOut: UILabel!
-    @IBOutlet weak var easeIn: UILabel!
+    @IBOutlet weak var ticker: UILabel!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    @IBOutlet weak var endValueLabel: UILabel!
+    @IBOutlet weak var endValueLabel: UITextField!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var durationSlider: UISlider!
     
-    var endValue: Int {
-        let value = pow(slider.value, 3)
-        let curvedValue = Int(value * 25000)
-        return curvedValue
+    var endValue: Double {
+        return Double(endValueLabel.text ?? "") ?? 0
     }
     
     var duration: TimeInterval {
@@ -32,7 +28,7 @@ class ViewController: UIViewController {
         return rounded
     }
     
-    var curve: QTickerCurve {
+    var curve: QuickTicker.AnimationCurve {
         switch segmentedControl.selectedSegmentIndex {
         case 0: return .linear
         case 1: return .easeIn
@@ -44,33 +40,69 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
-        
+        endValueLabel.delegate = self
+        ticker.font = UIFont.monospacedDigitSystemFont(ofSize: 20, weight: .medium)
         
     }
     
     @IBAction func startTicker(_ sender: UIButton) {
-        let startValue = Int(linear.text ?? "") ?? 0
-        
-//        linear.startTicker(duration: duration, until: endValue, curve: curve) { [unowned self] in
-//            self.linear.startTicker(duration: self.duration, until: startValue, curve: self.curve)
-//
-//        }
-        
-        QTicker.animate(label: linear, duration: duration, toEndValue: endValue, curve: curve) {
+        QuickTicker.animate(label: ticker, toEndValue: endValue, duration: duration, curve: curve) {
             print("VICTORY")
         }
-        
     }
     
     
     @IBAction func sliderChanged(_ sender: UISlider) {
-        endValueLabel.text = String(endValue)
+        let oldValue = Double(ticker.text ?? "") ?? 0
+        let value = pow(slider.value, 3)
+        let curvedValue = Double(value * 25000)
+        let decimalCount = getDecimalCount(input: oldValue)
+        if decimalCount > 0 {
+            endValueLabel.text = String(truncateNumber(curvedValue, toDecimals: decimalCount))
+        } else {
+            endValueLabel.text = String(Int(curvedValue))
+        }
     }
     
     @IBAction func durationSliderChanged(_ sender: UISlider) {
         durationLabel.text = String(duration)
     }
     
+    @IBAction func resetTapped(_ sender: UIButton) {
+        ticker.text = ""
+    }
+    
+    
 }
 
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension ViewController {
+    
+    private func truncateNumber(_ number: Double, toDecimals: Int) -> Double {
+        let power = pow(10, Double(toDecimals))
+        return Double(round(power * number) / power)
+    }
+    
+    private func getDecimalCount(input: Double) -> Int {
+        if digitIsInt(input) { return 0 }
+        let str = String(input)
+        if let dot = str.index(of: ".") {
+            let firstPart = str[str.startIndex...dot]
+            var decimals = str
+            decimals.removeFirst(firstPart.count)
+            return decimals.count
+        } else { return 0 }
+    }
+    
+    private func digitIsInt(_ digit: Double) -> Bool {
+        return Double(Int(digit)) == digit
+    }
+}
